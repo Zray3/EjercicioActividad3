794e97e504b7903066db0f162702148b80474002
Carlos Cremades <carloscremadesaleixandre@gmail.com>
2023-04-20 21:08:49 +0200

    1.0

--- app/src/main/java/es/ieslavereda/ejercicioactividad3/MainActivity.java
+++ app/src/main/java/es/ieslavereda/ejercicioactividad3/MainActivity.java
@@ -1,13 +1,18 @@
 package es.ieslavereda.ejercicioactividad3;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private TextView viewText;
@@ -16,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
     private RadioButton radioNa;
     private RadioButton radioNatacion;
     private RadioButton radioGames;
+    private Spinner spinnerView;
 
 
 
@@ -29,16 +35,32 @@ public class MainActivity extends AppCompatActivity {
         radioNatacion=findViewById(R.id.radioButtonNatacion);
         radioGames=findViewById(R.id.radioButtonGames);
         sendButton=findViewById(R.id.enviarBoton);
+        spinnerView=findViewById(R.id.spinner);
 
-        sendButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        ActivityResultLauncher<Intent> lanzadorActividadResultado=
+                registerForActivityResult(
+                        new ActivityResultContracts.StartActivityForResult(),
+                        result -> {
+                            if (result.getResultCode()==RESULT_CANCELED)
+                                Toast.makeText(this, "Cancelado por el usuario",
+                                        Toast.LENGTH_LONG).show();
+                            if (result.getResultCode()==RESULT_OK){
+                                Intent data = result.getData();
+                                String resultado =data.getExtras().getString("resultado");
+                                viewText.setText(resultado);
+                            }
+                        }
+                );
 
-            }
+        sendButton.setOnClickListener(view ->  {
+                Intent intent = new Intent(this, SecondActivity.class);
+                intent.putExtra("usuario", "Pepito Perez");
+                intent.putExtra("edad", 27);
+                lanzadorActividadResultado.launch(intent);
         });
         radioCiclismo.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick (View view) {
                 // whatever
             }
         });
